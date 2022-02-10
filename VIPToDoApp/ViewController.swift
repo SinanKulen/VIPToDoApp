87dc02706e1775ab3d8e90400cf3d48d5680e331
Sinan Kulen <sinan.kulen@gmail.com>
2022-02-10 23:08:12 +0300

    Initial Commit

---
+++ VIPToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPToDoApp
+//
+//  Created by Sinan Kulen on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
